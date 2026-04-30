93508d4346eedd641ca0913b45b2a093880f251b
Ishita Sinha <ishitasinha16@gmail.com>
2026-04-30 17:59:03 +0530

    Adding the top menu

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <span className="logo-icon">▣</span>
+        <span className="logo-text">DATA ANALYST</span>
+      </div>
+
+      <div className="navbar-links">
+        <a href="#" className="nav-link active">ABOUT ME</a>
+        <a href="#" className="nav-link">EXPERIENCE</a>
+        <a href="#" className="nav-link">PROJECTS</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
