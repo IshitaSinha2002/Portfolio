cf0c8399c3b358d974bcabbcf98ea6587656e861
Ishita Sinha <ishitasinha16@gmail.com>
2026-05-01 01:33:47 +0530

    Adding Experience section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
+import Education from "./components/Education";
 import "./index.css";
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
       <Skills />
       <Experience />
       <Projects />
+      <Education />
     </div>
   );
 };
